7b65ac014d89cb52ff702e427090e63fd1a2cf58
Artem <talibanssevik@gmail.com>
2022-04-26 20:00:55 +0300

    task2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -11,14 +11,16 @@ createGallery.insertAdjacentHTML('beforeend', cards)
 function createItem(cards) {
   return cards
     .map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
+      return `<div class="gallery__item">
+       
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        
       </div>`
   }).join('');
   
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,9 +9,9 @@ createGalleryBox.insertAdjacentHTML('beforeend', cards)
   
 function createItem(cards) {
   return cards.map(({preview, original, description})=> {
-    return`<a class="gallery__item" href="${original}">
+    return`<li><a class="gallery__item" href="${original}">
               <img class="gallery__image" src="${preview}" alt="${description}" />
-          </a>`
+          </a></li>`
   }).join('') 
 }
 
